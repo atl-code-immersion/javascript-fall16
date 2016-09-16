15259b3285fcb82040a25a3f94f58da666b3fb3b
Aaron Groch <ar.groch@gmail.com>
2016-09-16 13:29:31 -0400

    added jQuery UI functions

--- site.js
+++ site.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
 
+	$('#circle').click(function(){
+		// alert("You clicked the circle!");
+		$('p').addClass('yellow-bg');
+	});
+
 	$('#circle').hover(function(){
 		// alert("You clicked the circle!");
-		$('p').html("You clicked the circle!");
-	})
+		$('p').html("You hovered over the circle!");
+	});
 
 	$('.square').click(function(){
-		$(this).addClass('background', 'blue');
-	})
+		$(this).css('background', 'blue');
+	});
 
 	$(function() {
   	$('#draggable').draggable();
-  })
+  });
 
+	$(function() {
+  	$('#resizable').resizable();
+  });
+
+  $( function() {
+    $( "#sortable" ).sortable();
+    $( "#sortable" ).disableSelection();
+  });
 
 })
